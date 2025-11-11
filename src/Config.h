862ff63a671a374cec597a280c151dd292fd9733
woronin <woronin05@yandex.ru>
2025-11-11 18:03:08 +0300

    edit config.h file

--- src/Config.h
+++ src/Config.h
@@ -1,18 +1,13 @@
-//#define IF_VCPA
 #define IF_SWITCH
 #define AUTO_CONNECT
-//#define LASER
-//#define MQTT_LASER
-//#define MQTT_M6
 #define MQTT_HOMEASSIST
-//#define MQTT_ERR
 #define IFMQTT
 #define IF_AMT1001
 #define IF_DS18D20
 #define IF_PHandTEMP
 ////////////////////////// константы для алгоритма EC
 #define IF_EC_2DELAY                // алгоритм с двумя задержками (используем сейчас)
-//#define DEBUG_EC                    // вывод отладочной информации
+//#define DEBUG_EC                  // вывод отладочной информации
 //#define IF_EC_FILTER_SENSORS      // значение датчиков пропускаются через фильтр
 #define IF_EC_together_SENSORS      // два измерения без паузы (одно за другим)
 ///////////////////////////////////////////////////
@@ -24,6 +19,5 @@
 #define  HOMEASSIST_USE 
 #define  PAGE_OSCILLOSCOPE 
 #define  PAGE_DEBUG 
-//#define  PAGE_MQTT 
-//#define  DGTFILTERBAT 
+//#define  DGTFILTERBAT           // Цифровой фильтр
 #define  IF_FILTER_AMT1001 
